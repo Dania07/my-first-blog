3b7d4df2b424ddc00cb5c114af5eca7a3f6cb873
Dania07 <dania.ortiz@uabc.edu.mx>
2018-05-28 23:00:13 -0700

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -9,11 +9,13 @@ from .forms import PostForm
 def post_list (request):
     posts =Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'blog/post_detail.html', {'post' : post})
+
 def post_new(request):
-    if  request.method == "POST":
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
@@ -21,6 +23,10 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -32,5 +38,6 @@ def post_edit(request, pk):
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm(instance=post)
+        form  = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
